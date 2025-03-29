0983e62cb71a3a994ef53e0c87e56637401679a1
Hong Liu <106536555+ricardoliuhong@users.noreply.github.com>
2025-03-29 22:42:46 +0800

    Add files via upload

---
+++ Interface.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import scanpy as sc
+import warnings
+warnings.filterwarnings("ignore")
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 # enables correct plotting of text
+
+
+
+def Interface(h5ad_path, csv_path, output_h5ad_path):
+
+    adata = sc.read_h5ad(h5ad_path)
+    
+    # Read the CSV file
+    interface_data = pd.read_csv(csv_path)
+    
+    # Ensure data types are consistent
+    interface_data['X'] = interface_data['X'].astype(int)
+    interface_data['Y'] = interface_data['Y'].astype(int)
+    adata.obs['array_row'] = adata.obs['array_row'].astype(int)
+    adata.obs['array_col'] = adata.obs['array_col'].astype(int)
+    
+    # Merge InterfaceType annotation into adata.obs
+    merged_data = adata.obs.merge(interface_data[['X', 'Y', 'InterfaceType']], 
+                                  left_on=['array_row', 'array_col'], 
+                                  right_on=['X', 'Y'], 
+                                  how='left')
+    
+    # Drop rows where InterfaceType is NA
+    filtered_data = merged_data.dropna(subset=['InterfaceType'])
+    
+    # Drop unnecessary X and Y columns
+    filtered_data.drop(columns=['X', 'Y'], inplace=True)
+    
+    # Update adata.obs with the filtered data
+    adata.obs = filtered_data
+    
+    # Save the annotated AnnData object
+
+    adata.write_h5ad(output_h5ad_path)
+
+
